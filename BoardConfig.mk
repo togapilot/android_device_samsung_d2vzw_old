ca08a0f115738707d6ea88fd6d640582e9b2d913
Joel Flores <joelz9614@gmail.com>
2012-09-29 17:56:07 -0700

    fix inline kernel building

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,5 +30,5 @@
 TARGET_OTA_ASSERT_DEVICE := d2vzw
 
 # Kernel
-TARGET_KERNEL_CONFIG   := cyanogen_d2vzw_defconfig
-TARGET_KERNEL_SOURCE   := kernel/samsung/d2
+TARGET_KERNEL_SOURCE := kernel/samsung/d2
+TARGET_KERNEL_CONFIG := cyanogen_d2vzw_defconfig
